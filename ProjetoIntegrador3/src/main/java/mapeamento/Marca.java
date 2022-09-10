af89f8847433dba2bc3e105fb612dfe5803238c2
Krysaki <matheusreato12@hotmail.com>
2022-09-09 21:21:11 -0300

    mapeamento marca e modelo

--- ProjetoIntegrador3/src/main/java/mapeamento/Marca.java
+++ ProjetoIntegrador3/src/main/java/mapeamento/Marca.java
@@ -1,9 +1,86 @@
 package mapeamento;
 
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
  *
  * @author Matheus
  */
-public class Marca {
+
+@Entity
+@Table(name = "MARCA")
+public class Marca implements Serializable{
+    
+     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MARCA_SEQ")
+    @SequenceGenerator(name = "MARCA_SEQ", sequenceName = "MARCA_SEQ", allocationSize = 1)
+    @Column(name = "MARC_COD")
+    private Integer marcCod;
+    
+    @NotNull
+    @Size(max = 100)
+    @Column(name = "MARC_MARCA")
+    private String marcMarca;
+
+    public Marca(Integer marcCod, String marcMarca) {
+        this.marcCod = marcCod;
+        this.marcMarca = marcMarca;
+    }
+
+    public Integer getMarcCod() {
+        return marcCod;
+    }
+
+    public void setMarcCod(Integer marcCod) {
+        this.marcCod = marcCod;
+    }
+
+    public String getMarcMarca() {
+        return marcMarca;
+    }
+
+    public void setMarcMarca(String marcMarca) {
+        this.marcMarca = marcMarca;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 47 * hash + Objects.hashCode(this.marcCod);
+        hash = 47 * hash + Objects.hashCode(this.marcMarca);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Marca other = (Marca) obj;
+        if (!Objects.equals(this.marcMarca, other.marcMarca)) {
+            return false;
+        }
+        if (!Objects.equals(this.marcCod, other.marcCod)) {
+            return false;
+        }
+        return true;
+    }
+    
     
 }
--- ProjetoIntegrador3/src/main/java/mapeamento/Modelo.java
+++ ProjetoIntegrador3/src/main/java/mapeamento/Modelo.java
@@ -5,10 +5,109 @@
  */
 package mapeamento;
 
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
  *
  * @author Matheus
  */
-public class Modelo {
+
+@Entity
+@Table(name = "MODELO")
+public class Modelo implements Serializable{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MODELO_SEQ")
+    @SequenceGenerator(name = "MODELO_SEQ", sequenceName = "MODELO_SEQ", allocationSize = 1)
+    @Column(name = "MOD_COD")
+    private Integer modCod;
+    
+    @NotNull
+    @Size(max = 100)
+    @Column(name = "MOD_MODELO")
+    private String modModelo;
+    
+    @NotNull
+    @JoinColumn(name = "MOD_MARCCOD", referencedColumnName = "MARC_COD")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Modelo modMarcCod;
+
+    public Modelo(Integer modCod, String modModelo, Modelo modMarcCod) {
+        this.modCod = modCod;
+        this.modModelo = modModelo;
+        this.modMarcCod = modMarcCod;
+    }
+
+    public Integer getModCod() {
+        return modCod;
+    }
+
+    public void setModCod(Integer modCod) {
+        this.modCod = modCod;
+    }
+
+    public String getModModelo() {
+        return modModelo;
+    }
+
+    public void setModModelo(String modModelo) {
+        this.modModelo = modModelo;
+    }
+
+    public Modelo getModMarcCod() {
+        return modMarcCod;
+    }
+
+    public void setModMarcCod(Modelo modMarcCod) {
+        this.modMarcCod = modMarcCod;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 79 * hash + Objects.hashCode(this.modCod);
+        hash = 79 * hash + Objects.hashCode(this.modModelo);
+        hash = 79 * hash + Objects.hashCode(this.modMarcCod);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Modelo other = (Modelo) obj;
+        if (!Objects.equals(this.modModelo, other.modModelo)) {
+            return false;
+        }
+        if (!Objects.equals(this.modCod, other.modCod)) {
+            return false;
+        }
+        if (!Objects.equals(this.modMarcCod, other.modMarcCod)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
     
 }
