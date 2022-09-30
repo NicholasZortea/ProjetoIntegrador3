bbb41479cb4f69ee141660383b130f74cba4a07f
Krysaki <matheusreato12@hotmail.com>
2022-09-29 21:13:28 -0300

    commit

--- ProjetoIntegrador3/src/main/java/beans/CarroBean.java
+++ ProjetoIntegrador3/src/main/java/beans/CarroBean.java
@@ -3,6 +3,8 @@ package beans;
 import java.io.Serializable;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
 import mapeamento.Carro;
 import menu.bean.CarroService;
 
@@ -10,20 +12,46 @@ import menu.bean.CarroService;
  *
  * @author Gabriel
  */
+@Named
+@ViewScoped
 public class CarroBean implements Serializable {
 
     @EJB
     private CarroService carroService;
 
-    private Carro carro;
+    private String placa;
+    private int anoFabric;
 
     @PostConstruct
     private void init() {
         Carro carro = new Carro();
     }
-
+    
     public void salvar() {
+        Carro carro = new Carro();
+
+        carro.setCarPlaca(this.placa);
+        carro.setCarAnofabric(this.anoFabric);
+
         carroService.salvar(carro);
     }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public int getAnoFabric() {
+        return anoFabric;
+    }
+
+    public void setAnoFabric(int anoFabric) {
+        this.anoFabric = anoFabric;
+    }
+    
+    
 
 }
