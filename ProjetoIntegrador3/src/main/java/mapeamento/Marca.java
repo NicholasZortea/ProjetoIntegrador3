ca10f766150c9f73289506a93bdc6f9edbc108bf
Krysaki <matheusreato12@hotmail.com>
2022-10-07 22:30:10 -0300

    commit

--- ProjetoIntegrador3/src/main/java/mapeamento/Marca.java
+++ ProjetoIntegrador3/src/main/java/mapeamento/Marca.java
@@ -6,10 +6,13 @@ import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -22,10 +25,12 @@ import javax.validation.constraints.Size;
 @Table(name = "MARCA")
 public class Marca implements Serializable {
 
+   
     private static final long serialVersionUID = 1L;
     @Id
-    @Basic(optional = false)
-    @NotNull
+    //@Basic(optional = false)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MARCA_SEQ")
+    @SequenceGenerator(name = "CMARCA_SEQ", sequenceName = "MARCA_SEQ", allocationSize = 1)
     @Column(name = "MARC_COD")
     private Integer marcCod;
     @Basic(optional = false)
---
+++ ProjetoIntegrador3/src/main/java/service/AgendamentoService.java
@@ -0,0 +1,14 @@
+package service;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import mapeamento.Agenda;
+/**
+ *
+ * @author Gabriel
+ */
+@Stateless
+@LocalBean
+public class AgendamentoService extends Dao<Agenda> {
+
+}
